5c1c0c116ae0aceceaf5307ede5c4272a5ca0917
Ibrahim418 <brahimjaidane@gmail.com>
2021-07-06 22:10:55 +0100

    dsl ama ma9itelhech 7al ken hakek static 3alle5er

---
+++ models/Usermodel.js
@@ -0,0 +1,38 @@
+const mongoose=require('mongoose')
+
+const UserSchema= new mongoose.Schema({
+    firstname:String,
+    lastname:String,
+    phone:Number,
+    email:{type:String,
+      required:true},
+    password:String,
+    status: {
+        type: Boolean,
+        default: false,
+      },
+      permissionLevel: {
+        type: Number,
+        default: 1,
+      },
+      created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    blocking: {
+      type: Boolean,
+      default: false,
+    },
+    bio:{
+      type:String
+    },
+    avatar: {
+      type: String,
+      default:'./images/capserrat.jpg'
+    },
+  }
+);
+module.exports=mongoose.model('User',UserSchema);
+
+//--------------------------------------------------------
+
